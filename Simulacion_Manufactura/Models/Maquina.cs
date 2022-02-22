67d260b18ff9d45065097c24113c1da131be516c
roger <rogerar@isppro.net>
2022-02-21 22:35:09 -0600

    Added Objects

---
+++ Simulacion_Manufactura/Models/Maquina.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simulacion_Manufactura.Models
+{
+    public class Maquina
+    {
+        public string id { get; set; }
+        public int cantProdHoras { get; set; }
+        public decimal costoHora { get; set; }
+        public decimal probabilidadFallo { get; set; }
+    }
+}
---
+++ Simulacion_Manufactura/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simulacion_Manufactura.Models
+{
+    public class Producto
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public decimal precio { get; set; }
+    }
+}
